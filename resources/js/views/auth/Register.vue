225945e7914ff6fe21a96ad559860562fd0c757f
mercExec <defaultg123@gmail.com>
2023-11-26 13:31:40 +0300

    Added required to terms & conditions checkbox

--- resources/js/views/auth/Register.vue
+++ resources/js/views/auth/Register.vue
@@ -53,7 +53,7 @@
                     </div>
                     <div class="flex items-start">
                         <div class="flex items-center h-5">
-                            <input id="terms" aria-describedby="terms" type="checkbox"
+                            <input id="terms" aria-describedby="terms" type="checkbox" required
                                 class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                         </div>
                         <div class="ml-3 text-sm">
